387d6fb13e248970b1530a12ad8872ada5094b1e
leighd2008 <leighd2008@gmail.com>
2017-08-18 08:07:37 -0400

    App finished

--- DDP-Week4/server.R
+++ DDP-Week4/server.R
@@ -10,69 +10,109 @@
 library(shiny)
 library(ggplot2)
 library(data.table)
+library(dplyr)
 
-# Define server logic required to draw a histogram
+# Define server logic required to draw plot
 shinyServer(function(input, output) {
+     #Get data, create column combining location and treatment
    ds <- as.data.table(CO2)
    ds <- ds[, ':='(comb=paste(Type, "-", Treatment))]
-   qbc <- ds[Type == "Quebec"]
-   msp <- ds[Type == "Mississippi"]
-   mspch <- msp[Treatment == "chilled"]
-   mspnonch <- msp[!Treatment == "chilled"]
-   qbcch <- qbc[Treatment == "chilled"]
-   qbcnonch <- qbc[!Treatment == "chilled"]
    
-# Fits for each location-treatment combination   
+   ## Create a 4 color mini palatte
+   gg_color_hue <- function(n){
+        hues = seq(15, 375, length = n + 1)
+        hcl(h = hues, l = 65, c = 100)[1:n]
+   }
+   cols <- gg_color_hue(4)
+   cols2 <- cols[3:4]
+   
+## Fits for each location-treatment combination were defined outside of the shiny
+## app using polynomial regression  
    mspchfit= lm(uptake~conc ,data=ds[comb == "Mississippi - chilled"])
-   mspnonchfit= lm(uptake~poly(conc ,4) ,data=mspnonch)
-   qbcchfit= lm(uptake~poly(conc ,4) ,data=qbcch)
-   qbcnonchfit= lm(uptake~poly(conc ,5) ,data=qbcnonch)
+   mspnonchfit= lm(uptake~poly(conc ,4) ,data=ds[comb == "Mississippi - nonchilled"])
+   qbcchfit= lm(uptake~poly(conc ,4) , data=ds[comb == "Quebec - chilled"])
+   qbcnonchfit= lm(uptake~poly(conc ,5) , data=ds[comb == "Quebec - nonchilled"])
+   
+   model1pred <- reactive({
+     CO2Input <- input$sliderCO2
+     predict(mspchfit, newdata = data.frame(conc = CO2Input))
+   })
+   model2pred <- reactive({
+     CO2Input <- input$sliderCO2
+     predict(mspnonchfit, newdata = data.frame(conc = CO2Input))
+   })
+   model3pred <- reactive({
+     CO2Input <- input$sliderCO2
+     predict(qbcchfit, newdata = data.frame(conc = CO2Input))
+   })
+   model4pred <- reactive({
+     CO2Input <- input$sliderCO2
+     predict(qbcnonchfit, newdata = data.frame(conc = CO2Input))
+   })
    
   output$plot1 <- renderPlot({
     
     # Plot CO2 uptake versus CO2 concentration
      if(input$location == 3){
-          g <- ggplot(ds, aes(x = conc, y = uptake, size = 2)) +
-               theme_bw() + guides(size = FALSE) + geom_point(aes(color = comb))
-          g <- g + geom_smooth(method = "lm", lwd = 2, se = FALSE,
-               data = ds[comb == "Mississippi - chilled"],  color="salmon") +
+          g <- ggplot(ds, aes(x = conc, y = uptake, color = comb, size =2)) +
+               theme_bw() + guides(size = FALSE) + geom_point() 
                
-               
-               
-               
-               
-               geom_smooth(aes(conc, uptake), method = "lm", 
-                    ds[comb =="Mississippi - nonchilled"],lwd = 2, se = FALSE) + 
-               geom_smooth(aes(conc, uptake), method = "lm", 
-                    ds[comb == "Quebec - chilled"], lwd = 2, se = FALSE) +
-               geom_smooth(aes(conc, uptake), method = "lm", 
-                    ds[comb == "Quebec - nonchilled"], lwd = 2, se = FALSE)
+          g <- g + 
+               geom_smooth(method = "lm", formula = y ~ x, lwd = 2, 
+                    se = FALSE, color = cols[1], 
+                    data = filter(ds, comb == "Mississippi - chilled")) +
+               geom_smooth(method = "lm", formula = y ~ poly(x, 4), lwd = 2, 
+                    se = FALSE, color = cols[2],  
+                    data = filter(ds, comb == "Mississippi - nonchilled")) +
+               geom_smooth(method = "lm", formula = y ~ poly(x, 4), lwd = 2, 
+                    se = FALSE, color = cols[3],  
+                    data = filter(ds, comb == "Quebec - chilled")) +
+               geom_smooth(method = "lm", formula = y ~ poly(x, 5), lwd = 2, 
+                    se = FALSE, color = cols[4], 
+                    data = filter(ds, comb == "Quebec - nonchilled"))
           g
      }else{
-          if(input$location == 2){
-               g <- ggplot(msp, aes(x = conc, y = uptake, size = 2)) +
-                    theme_bw() + guides(size = FALSE)
-               g <- g + geom_point(aes(color = Treatment)) + 
-                         geom_smooth(aes(conc, uptake), method = "lm", mspch,
-                                     color = "turquoise3", lwd = 2, se = FALSE) +
-                         geom_smooth(aes(conc, uptake), method = "lm", mspnonch,
-                                     color = "salmon", lwd = 2, se = FALSE)
+          if(input$location == 1){
+               g <- ggplot(data = filter(ds, Type == "Mississippi"), 
+                    aes(x = conc, y = uptake, color = comb, size =2)) + 
+                    theme_bw() + guides(size = FALSE) + geom_point() 
+               g <- g + 
+                    geom_smooth(method = "lm", formula = y ~ x, lwd = 2, 
+                         se = FALSE, color = cols[1],  
+                         data = filter(ds, comb == "Mississippi - chilled")) +
+                    geom_smooth(method = "lm", formula = y ~ poly(x, 4), 
+                         lwd = 2, se = FALSE, color = cols[3],  
+                         data = filter(ds, comb == "Mississippi - nonchilled"))
                g
           }else{
-          if(input$location == 1){
-               g <- ggplot(qbc, aes(x = conc, y = uptake, size = 2)) +
-                    theme_bw() + guides(size = FALSE)
-               g <- g + geom_point(aes(color = Treatment, 
-                                     scale_color_manual(values = cbPallet))) + 
-                         geom_smooth(aes(conc, uptake), method = "lm", qbcch,
-                                     color = "turquoise3", lwd = 2, se = FALSE) +
-                         geom_smooth(aes(conc, uptake), method = "lm", qbcnonch,
-                                     color = "salmon", lwd = 2, se = FALSE)
+          if(input$location == 2){
+               g <- ggplot(data = filter(ds, Type == "Quebec"), 
+                    aes(x = conc, y = uptake, color = comb, size =2)) + 
+                    theme_bw() + guides(size = FALSE) + geom_point() 
+               g <- g + 
+                    geom_smooth(method = "lm", formula = y ~ poly(x, 4), lwd = 2, 
+                         se = FALSE, color = cols[1],  
+                         data = filter(ds, comb == "Quebec - chilled")) +
+                    geom_smooth(method = "lm", formula = y ~ poly(x, 5), 
+                         lwd = 2, se = FALSE, color = cols[3],  
+                         data = filter(ds, comb == "Quebec - nonchilled"))
                g
           }
           }
      }
      
+  })
+  output$pred1 <- renderText({
+    model1pred()
+  })
+  output$pred2 <- renderText({
+    model2pred()
+  })
+  output$pred3 <- renderText({
+    model3pred()
+  })
+  output$pred4 <- renderText({
+    model4pred()
   })
   
 })
--- DDP-Week4/ui.R
+++ DDP-Week4/ui.R
@@ -10,31 +10,41 @@
 library(shiny)
 
 # Define UI for application that explores CO2 uptake data from plants.
-shinyUI(fluidPage(
-  
-  # Application title
-  titlePanel("CO2 Uptake by plants"),
-  
-  # Sidebar with radio buttons to choose location of plant(Quebec or Mississippi) 
-  sidebarLayout(
-    sidebarPanel(
-      radioButtons("location", label = 
-        h3("Choose to see the data from Quebec, Mississippi or Both together."),
-        choices = list("Quebec"= 1, "Mississippi"= 2, "Both"= 3), selected = 3),
-     h3("Lines"),
-     helpText("The line represent the best polynomial regression fit for each 
-               location - treatment combination. This will allow us to predict 
-               an uptake value for a given ambient concentration of CO2 on the 
-               Predict tab.")
-     ),
-    
-     # Show a plot of the CO2 data.
-    mainPanel(
-         tabsetPanel(type = "tabs",
-                     tabPanel("Exploratory Plots", br(), plotOutput("plot1")),
-                     tabPanel("Predict", br(), textOutput("out2"))
+navbarPage("CO2 Uptake by plants",
+   tabPanel("Exploratory Plots",
+      # Sidebar with radio buttons to choose location of plant(Quebec or Mississippi) 
+      sidebarLayout(
+         sidebarPanel(
+            radioButtons("location", label = 
+               h3("Choose to see the data from Quebec, Mississippi or Both together."),
+                  choices = list("Mississippi"= 1,"Quebec"= 2,  "Both"= 3), selected = 3),
+               h3("Lines"),
+               helpText("The lines represent the best polynomial regression fit for each 
+                  location - treatment combination. This will allow us to predict 
+                  an uptake value for a given ambient concentration of CO2 on the 
+                  Predict tab.")
+         ),
+         mainPanel(
+            plotOutput("plot1")
          )
-    )
-    
-  )
-))
+      )
+   ),
+   tabPanel("Predict",
+      sidebarLayout(
+        sidebarPanel(
+          sliderInput("sliderCO2", "What ambient CO^2 concentration would you 
+                            like to explore?", 0, 1000, value = 500)
+                ),
+        mainPanel(
+          HTML(paste(tags$h5("Predicted CO ", tags$sup(2), " uptake for plants in Mississippi that have been chilled:"))),
+          textOutput("pred1"),
+          HTML(paste(tags$h5("Predicted CO ", tags$sup(2), " uptake for plants in Mississippi that have not been chilled:"))),
+          textOutput("pred2"),
+          HTML(paste(tags$h5("Predicted CO ", tags$sup(2), " uptake for plants in Quebec that have been chilled:"))),
+          textOutput("pred3"),
+          HTML(paste(tags$h5("Predicted CO ", tags$sup(2), " uptake for plants in Quebec that have not been chilled:"))),
+          textOutput("pred4")
+        )
+   )
+)
+)
